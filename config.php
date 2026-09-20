a2af8d015c8edf880f183db57886b189e5c68547
Madhan33 <madhanpachamuthu33@gmail.com>
2026-09-20 10:38:27 +0530

    Add SSL options for PDO MySQL connection

--- config.php
+++ config.php
@@ -32,6 +32,8 @@ $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
+    PDO::MYSQL_ATTR_SSL_CA       => __DIR__ . '/ca.pem',
+    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true,
 ];
 
 try {
