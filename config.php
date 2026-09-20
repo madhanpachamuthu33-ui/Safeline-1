62eeb2ba29ae994a9637d772203db2c385ddc6c4
Madhan33 <madhanpachamuthu33@gmail.com>
2026-09-20 10:17:07 +0530

    Update config.php

--- config.php
+++ config.php
@@ -27,7 +27,7 @@ $DB_PASS = getenv('DB_PASS') ?: '';
 $DB_PORT = getenv('DB_PORT') ?: '3306';
 $DB_CHARSET = 'utf8mb4';
 
-$dsn = "mysql:host=$DB_HOST;dbname=$DB_NAME;charset=$DB_CHARSET";
+$dsn = "mysql:host=$DB_HOST;port=$DB_PORT;dbname=$DB_NAME;charset=$DB_CHARSET";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
